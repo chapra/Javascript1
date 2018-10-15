d3c8ea6a9d4e6378a0dbb1fe34f9feff1be56925
Pranitha <cvpranita@yahoo.co.in>
2018-10-15 12:19:26 +0200

    homework extension

---
+++ task5.js
@@ -0,0 +1,10 @@
+let z =7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+
+if (a>z){
+    console.log('this is the highest value ' + a);
+} else {
+    console.log('this is the highest value ' + z);
+}
---
+++ task6.js
@@ -0,0 +1,7 @@
+let array = [ ];
+console.log('I think the value of the array will be Undefined');
+console.log(array);
+let favouriteAnimal = [ "cats","dogs", "birds"];
+console.log(favouriteAnimal);
+favouriteAnimal.push("baby pig") ;
+console.log(favouriteAnimal);
